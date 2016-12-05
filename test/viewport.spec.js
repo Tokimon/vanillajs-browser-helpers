b0b96ed42801e693cc8103d0069c69e2eecf8c4c
Toke Voltelen <tokevoltelen@gmail.com>
2016-12-05 14:38:08 +0100

    viewport: Correct testcase to reflect updated isWindow/isDOMDocument methods

--- test/viewport.spec.js
+++ test/viewport.spec.js
@@ -4,16 +4,19 @@
 import viewport from '../viewport';
 
 describe('"viewport"', () => {
-  it('Should return NULL if no document was found', () => {
-    expect(viewport({})).to.be.null;
-  });
+  const { body, documentElement: html } = document;
+  const bodyOrHtml = (elm) => elm === body || elm === html;
 
-  it('Should return either the Body or DOM element', () => {
-    const { body, documentElement: html } = document;
-    const bodyOrHtml = (elm) => elm === body || elm === html;
+  it('Should return NULL if no document was found', () => {
     const fakeDocCompat = { nodeType: 9, compatMode: 'BackCompat', body };
     const fakeDoc = { nodeType: 9, documentElement: html };
 
+    expect(viewport({})).to.be.null;
+    expect(viewport(fakeDoc)).to.be.null;
+    expect(viewport(fakeDocCompat)).to.be.null;
+  });
+
+  it('Should return either the Body or DOM element', () => {
     expect(viewport(document.createElement('div'))).to.satisfy(bodyOrHtml);
     expect(viewport(document.createComment('Comment'))).to.satisfy(bodyOrHtml);
     expect(viewport(document.body)).to.satisfy(bodyOrHtml);
@@ -22,7 +25,5 @@ describe('"viewport"', () => {
     expect(viewport(document)).to.satisfy(bodyOrHtml);
     expect(viewport(null)).to.satisfy(bodyOrHtml);
     expect(viewport()).to.satisfy(bodyOrHtml);
-    expect(viewport(fakeDoc)).to.satisfy(bodyOrHtml);
-    expect(viewport(fakeDocCompat)).to.satisfy(bodyOrHtml);
   });
 });
