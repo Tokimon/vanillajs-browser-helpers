1cb3626831f5f7dd732ce118a534de1c655ec39d
Toke Voltelen <tokevoltelen@gmail.com>
2016-12-05 12:30:44 +0100

    isWindow: Improve check

--- isWindow.js
+++ isWindow.js
@@ -1,3 +1,9 @@
+/**
+ * Is the given object a Window object
+ * @param  {Object} obj - The object to check
+ * @return {Boolean} - Is it a Window object or not
+ */
 export default function isWindow(obj) {
-  return !!obj && typeof obj.self !== 'undefined' && obj.self === obj;
+  if(!obj || !obj.self) { return false; }
+  return obj instanceof obj.self.Window;
 }
--- test/isWindow.spec.js
+++ test/isWindow.spec.js
@@ -4,18 +4,17 @@
 import isWindow from '../isWindow';
 
 describe('"isWindow"', () => {
-  before(() => $.html('<iframe id="iFrame">'));
-  after(() => $.remove('iFrame'));
-
   it('Should return true for window elements', () => {
-    const iframe = $.id('iFrame');
     expect(isWindow(window)).to.be.true;
+
+    const iframe = $.create('iframe');
+    iframe.src = 'about:blank';
+    document.body.appendChild(iframe);
+
     expect(isWindow(iframe)).to.be.false;
     expect(isWindow(iframe.contentWindow)).to.be.true;
 
-    const fake = {};
-    fake.self = fake;
-    expect(isWindow(fake)).to.be.true;
+    document.body.removeChild(iframe);
   });
 
   it('Should return false for non window values', () => {
