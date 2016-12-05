0d0db554c10f6601d7005eeeaab1a498a88bfa1b
Toke Voltelen <tokevoltelen@gmail.com>
2016-12-05 12:30:31 +0100

    isDOMDocument: Improve check

--- isDOMDocument.js
+++ isDOMDocument.js
@@ -1,3 +1,9 @@
-export default function isDOMDocument(elm) {
-  return !!elm && elm.nodeType === 9;
+/**
+ * Is the given object a DOM document node
+ * @param {Object} obj - The object to check
+ * @return {Boolean} - Is it a DOM document node or not
+ */
+export default function isDOMDocument(obj) {
+  if(!obj || !obj.defaultView) { return false; }
+  return obj instanceof obj.defaultView.Document;
 }
--- test/isDOMDocument.spec.js
+++ test/isDOMDocument.spec.js
@@ -6,7 +6,14 @@ import isDOMDocument from '../isDOMDocument';
 describe('"isDOMDocument"', () => {
   it('Should only return true for Document node elements', () => {
     expect(isDOMDocument(document)).to.be.true;
-    expect(isDOMDocument({ nodeType: 9 })).to.be.true;
+
+    const iframe = $.create('iframe');
+    iframe.src = 'about:blank';
+    document.body.appendChild(iframe);
+
+    expect(isDOMDocument(iframe.contentDocument)).to.be.true;
+
+    document.body.removeChild(iframe);
   });
 
   it('Should return false for non Document node elements', () => {
@@ -15,6 +22,7 @@ describe('"isDOMDocument"', () => {
     expect(isDOMDocument(document.body)).to.be.false;
     expect(isDOMDocument(null)).to.be.false;
     expect(isDOMDocument({})).to.be.false;
+    expect(isDOMDocument({ nodeType: 9 })).to.be.false;
     expect(isDOMDocument()).to.be.false;
   });
 });
