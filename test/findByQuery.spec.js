147bc57a3605a19aee84900a70ea2deb8cdb26b3
Toke Voltelen <tokevoltelen@gmail.com>
2017-03-15 11:53:33 +0100

    Linting improvements

--- test/findByQuery.spec.js
+++ test/findByQuery.spec.js
@@ -1,4 +1,5 @@
 /* eslint-env node, mocha, browser */
+/* eslint-disable no-unused-expressions */
 /* global expect, $ */
 import 'polyfills/array-from-polyfill';
 import findByQuery from '../findByQuery';
