d670502dc48a004d1036141f9e48676218698d4e
Toke Voltelen <toke.voltelen@yoomap.fr>
2018-02-27 20:14:53 +0100

    inDOM: Use document.contains()

--- inDOM.js
+++ inDOM.js
@@ -7,5 +7,5 @@ import isDOMNode from './isDOMNode';
  * @return {Boolean} Is it a DOM node in the DOM or not
  */
 export default function inDOM(elm) {
-  return isDOMNode(elm) && !!(elm.offsetParent || elm.offsetHeight || elm.offsetWidth);
+  return isDOMNode(elm) && document.contains(elm);
 }
