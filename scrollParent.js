f3a43a7b23f0b9eda539fba4b654cd87c874d8fd
Toke Voltelen <toke.voltelen@yoomap.fr>
2018-02-27 20:13:24 +0100

    Create scrollParent

---
+++ scrollParent.js
@@ -0,0 +1,28 @@
+import viewport from './viewport';
+import isDOMChildNode from './isDOMChildNode';
+
+export default function scrollParent(elm) {
+  const { position: elmPosition } = getComputedStyle(elm);
+
+  if(isDOMChildNode(elm) && elmPosition !== 'fixed') {
+    const noStaticParent = elmPosition === 'absolute';
+    let parent = elm;
+
+    while(parent) {
+      if(parent === document.body) { break; }
+
+      const { position, overflow, overflowX, overflowY } = getComputedStyle(parent);
+
+      if(
+        !(noStaticParent && position === 'static') &&
+        /(auto|scroll)/.test(overflow + overflowY + overflowX)
+      ) {
+        return parent;
+      }
+
+      parent = parent.parentElement;
+    }
+  }
+
+  return viewport(window);
+}
