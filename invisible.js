3c6998c46f11d077c4d40f3ac768076de9b8ee25
Toke Voltelen <toke.voltelen@yoomap.fr>
2018-02-27 20:16:15 +0100

    invisible: use hidden check

--- invisible.js
+++ invisible.js
@@ -10,9 +10,9 @@ export default function invisible(elm) {
   if(hidden(elm)) { return true; }
 
   // In collapsed or transparent elements we have to look at the element and its
-  // paranets to see if we finde one that is invisible
+  // parents to see if we find one that is invisible
   while(elm) {
-    if(!elm.offsetHeight || !elm.offsetWidth || !Number(getComputedStyle(elm).opacity)) { return true; }
+    if(hidden(elm) || !Number(getComputedStyle(elm).opacity)) { return true; }
     elm = elm.parentElement;
   }
 
