e1e6395fa072a8881a69d13115dcc99c9f5d7392
Toke Voltelen <toke.voltelen@yoomap.fr>
2018-02-28 11:38:10 +0100

    findByClass: prettify a bit

--- findByClass.js
+++ findByClass.js
@@ -2,30 +2,30 @@ import isString from 'vanillajs-helpers/isString';
 import isArray from 'vanillajs-helpers/isArray';
 import iterate from 'vanillajs-helpers/iterate';
 
+
+
 /**
- * Finds DOM elements with a given class name
+ * Finds DOM elements with a given class name.
+ * Seperate multiple selectors by comma. Seperate multiple class names by space.
  * @function findByClass
  * @param {String|String[]} classNames - Class name to find elements by
- * @param {HTMLElement} [elm=document] - The DOM element to start the search from
+ * @param {HTMLElement} [context=document] - The DOM element to start the search from
  * @return {HTMLElement[]} List of found DOM elements
  */
-export default function findByClass(classNames, elm) {
-  // Is it is a string split by comma (convert to Array)
-  if(isString(classNames)) { classNames = classNames.replace(/\./g, ' ').split(/\s*[,]\s*/); }
-
-  // 'clasNames' has to be an Array
+export default function findByClass(classNames, context) {
+  if(isString(classNames)) { classNames = classNames.replace(/\./g, ' ').split(/[,]+/); }
   if(!isArray(classNames)) { return []; }
 
-  // 'elm' must be an object with the 'getElementsByClassName' implementation
-  if(!elm || !elm.getElementsByClassName) { elm = document; }
+  if(!context || !context.getElementsByClassName) { context = document; }
 
-  // If only one expression have been passed in return the result as an Array
-  if(classNames.length < 2) { return Array.from(elm.getElementsByClassName(classNames[0])); }
+  if(classNames.length < 2) {
+    return Array.from(context.getElementsByClassName(classNames[0]));
+  }
 
   // If several expressions have been passed in
   // we need to create an unique array of the found nodes
   return Array.from(classNames.reduce((set, cn) => {
-    iterate(elm.getElementsByClassName(cn), (node) => set.add(node));
+    iterate(context.getElementsByClassName(cn), (node) => set.add(node));
     return set;
   }, new Set()));
 }
