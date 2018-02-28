70cabf52cb4a58a4f31fe401a019fbe222c1ad60
Toke Voltelen <toke.voltelen@yoomap.fr>
2018-02-28 18:06:11 +0100

    remove: Prettify code a bit

--- remove.js
+++ remove.js
@@ -7,6 +7,5 @@ import isDOMChildNode from './isDOMChildNode';
  * @return {HTMLElement} The given `elm` value
  */
 export default function remove(elm) {
-  if(isDOMChildNode(elm)) { return elm.parentNode.removeChild(elm); }
-  return elm;
+  return isDOMChildNode(elm) ? elm.parentNode.removeChild(elm) : elm;
 }
