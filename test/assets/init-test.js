429a58554f6ee2dca4513de41a2e532d50225bcd
Toke Voltelen <tokevoltelen@gmail.com>
2018-10-24 10:58:52 +0200

    Init Tests: better exports

--- test/assets/init-test.js
+++ test/assets/init-test.js
@@ -1,24 +1,27 @@
-/* global chai, describe, it, before, beforeEach, after, afterEach */
+/* global  describe, it, before, beforeEach, after, afterEach */
+
+import { expect, use } from 'chai';
+import chaiDom from 'chai-dom';
+import sinonChai from 'sinon-chai';
 
 
-// --- Mocha ---
-export const describe = window.describe;
-export const it = window.it;
-export const before = window.before;
-export const beforeEach = window.beforeEach;
-export const after = window.after;
-export const afterEach = window.afterEach;
+// --- Chai setup ---
+use(chaiDom);
+use(sinonChai);
+export { expect };
 
+// --- Mocha exports ---
+const { describe, it, before, beforeEach, after, afterEach } = window;
+export { describe, it, before, beforeEach, after, afterEach };
 
-
-// --- Chai expect ---
-export const expect = window.chai.expect;
+// --- Sinon exports ---
+export { spy, stub, fake, match as sinonMatch } from 'sinon';
 
 
 
 // --- Test Utils ---
 function _createEvent(eventName, data) {
-  if('CustomEvent' in window) {
+  if ('CustomEvent' in window) {
     return new window.CustomEvent(eventName, { detail: data, bubbles: true });
   } else {
     const evt = document.createEvent('CustomEvent');
@@ -27,18 +30,25 @@ function _createEvent(eventName, data) {
   }
 }
 
-export const testUtils = {
+export const helpers = {
   id(id) { return document.getElementById(id); },
+
   query(query, elm) { return (elm || document).querySelectorAll(query); },
-  remove(id, elm) { try { (elm || document.body).removeChild(_id(id)); } catch(ex) { /* Fail silently */ } },
+
+  remove(id, elm) { try { (elm || document.body).removeChild(id); } catch (ex) { /* Fail silently */ } },
+
   create(tagName) { return document.createElement(tagName); },
+
   html(html, elm) { (elm || document.body).insertAdjacentHTML('beforeend', html); },
+
   trigger(eventName, elm) {
     (elm || document).dispatchEvent(_createEvent(eventName));
   },
+
   on(elm, evt, handler) {
     elm.addEventListener(evt, handler, false);
   },
+
   off(elm, evt, handler) {
     elm.removeEventListener(evt, handler, false);
   }
