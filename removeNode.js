7d9dee91da59ee200dabb6e80c0952ac08d1b791
Toke Voltelen <tokevoltelen@gmail.com>
2018-12-21 23:12:16 +0100

    Rename `remove` to `removeNode`

--- removeNode.js
+++ removeNode.js
@@ -4,8 +4,8 @@ import isDOMChildNode from './isDOMChildNode';
  * Remove a given DOM element from the DOM
  * @function remove
  * @param {HTMLElement} elm - The DOM element to remove
- * @return {HTMLElement} The given `elm` value
+ * @return {HTMLElement} The given `elm`
  */
-export default function remove(elm) {
+export default function removeNode(elm) {
   return isDOMChildNode(elm) ? elm.parentNode.removeChild(elm) : elm;
 }
---
+++ test/removeNode.spec.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-unused-expressions */
+
+import { expect, helpers, describe, it, after } from './assets/init-test';
+
+import removeNode from '../removeNode';
+
+
+
+const testID = 'testNode';
+
+const appendTestNode = () => {
+  removeTestNode();
+  helpers.html(`<div id="${testID}"></div>`);
+  return helpers.id(testID);
+};
+
+const removeTestNode = () => helpers.remove('testNode');
+
+
+
+describe('"removeNode"', () => {
+  after(removeTestNode);
+
+  it('Should ignore non DOM child elements', () => {
+    try {
+      removeNode(null);
+      removeNode(document);
+      removeNode(document.documentElement);
+      removeNode(window);
+    } catch (ex) {
+      expect.fail();
+    }
+  });
+
+  it('Should remove a given DOM element', () => {
+    const testNode = appendTestNode();
+
+    removeNode(testNode);
+    expect(helpers.id(testID)).to.equal(null);
+    expect(testNode.parentNode).to.equal(null);
+  });
+
+  it('Should always return the removed element', () => {
+    const testNode = appendTestNode();
+
+    expect(removeNode(null)).to.equal(null);
+    expect(removeNode(document)).to.equal(document);
+    expect(removeNode(window)).to.equal(window);
+    expect(removeNode(testNode)).to.equal(testNode);
+  });
+});
