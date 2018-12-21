1a1f7d227dedf665ee8a886b34085f15bdc9729d
Toke Voltelen <tokevoltelen@gmail.com>
2018-12-21 22:43:45 +0100

    Update `css`

--- css.js
+++ css.js
@@ -5,40 +5,35 @@ import dashed from 'vanillajs-helpers/dashed';
 import isDOMElement from './isDOMElement';
 
 /**
- * Get current styling of a DOM element and optionally set given style first
+ * Get current computed styling from a DOM element. it takes a second argument for which
+ * there are two possibilities:
+ * - Object ({ [prop]: 'value' }): those styles are set on the element before returning the computed style
+ * - String: Return only the value of that specific property
  * @function css
  * @param {HTMLElement} elm - DOM element to get the style from
- * @param {Object} [pseudo] - Pseudo element to get the styling from
- * @param {Object} [style] - Styling to set on the element
+ * @param {Object|string} [style] - Styling to set on the element (object)
+ *                                  or style property to get from the element (string)
  * @return {Object|null} Current styling on the element
  */
-export default function css(elm, pseudo, style) {
-  if(!isDOMElement(elm)) { return null; }
-
-  // TODO: Find a way to return null when pseudo is defined but there is no pseudo element for the element
-  if(/^[:]{0,2}(before|after)$/.test(pseudo)) {
-    pseudo = pseudo.replace(/^[:]*/g, ':');
-    if(!isString(style)) { style = null; }
-  } else {
-    style = pseudo;
-    pseudo = null;
-  }
+export default function css(elm, style) {
+  if (!isDOMElement(elm)) { return null; }
 
   // If styles are defined, then add them to the elments inline style
-  if(isObject(style)) {
+  if (isObject(style)) {
     // Go through each style
     Object.keys(style).forEach((key) => {
       // Set the style
-      const val = style[key].match(/(.*)(?:\s+[!](important))?$/);
-      elm.style.setProperty(dashed(key), val[1], val[2] || '');
+      const val = style[key].match(/^(.+)(?:\s+[!](important))?$/);
+
+      if (val) {
+        elm.style.setProperty(dashed(key), val[1], val[2] || '');
+      }
     });
   }
 
+  const computed = window.getComputedStyle(elm);
 
-  const computed = window.getComputedStyle(elm, pseudo);
-  if(!isString(style)) { return computed; }
-
-  const val = computed.getPropertyValue(dashed(style));
-  if(!val) { return null; }
-  return style === 'content' ? `${val}`.replace(/^"|"$/g, '') : val;
+  return isString(style)
+    ? computed.getPropertyValue(dashed(style)) || null
+    : computed;
 }
--- test/css.spec.js
+++ test/css.spec.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-expressions */
-
 import { expect, helpers, describe, it, before, beforeEach, after } from './assets/init-test';
 
 import css from '../css';
@@ -11,12 +9,18 @@ const testID = 'TestNode';
 
 
 describe('"css"', () => {
-  before(() => helpers.html(`
-    <style id="style">#${testID} { overflow: hidden; font-size: 15px; } #${testID}:after { content: 'after'; }</style>
-    <div id="${testID}"></div>
-  `));
+  let testNode;
 
-  beforeEach(() => helpers.id(testID).removeAttribute('style'));
+  before(() => {
+    helpers.html(`
+      <style id="style">#${testID} { overflow: hidden; font-size: 15px; } #${testID}:after { content: 'after'; }</style>
+      <div id="${testID}"></div>
+    `);
+
+    testNode = helpers.id(testID);
+  });
+
+  beforeEach(() => testNode.removeAttribute('style'));
 
   after(() => {
     helpers.remove(testID);
@@ -24,50 +28,39 @@ describe('"css"', () => {
   });
 
   it('Should read the current style of a DOM element', () => {
-    const node = helpers.id(testID);
-    const styling = css(node);
+    const styling = css(testNode);
     expect(styling).to.not.equal(null);
     expect(styling.overflow).to.equal('hidden');
   });
 
   it('Should get the value of the given property from the style', () => {
-    const node = helpers.id(testID);
-    node.style.lineHeight = '15px';
-    node.style.fontSize = '15px';
-    expect(css(node, 'line-height')).to.equal('15px');
-    expect(css(node, 'fontSize')).to.equal('15px');
-    expect(css(node, 'overflow')).to.equal('hidden');
-    expect(css(node, 'not-a-css-prop')).to.equal(null);
+    testNode.style.lineHeight = '15px';
+    testNode.style.fontSize = '15px';
+
+    expect(css(testNode, 'line-height')).to.equal('15px');
+    expect(css(testNode, 'fontSize')).to.equal('15px');
+    expect(css(testNode, 'overflow')).to.equal('hidden');
+    expect(css(testNode, 'not-a-css-prop')).to.equal(null);
   });
 
   it('Should change the styling of a DOM element', () => {
-    const node = helpers.id(testID);
-    const newstyling = css(node, { height: '45px' });
+    const newstyling = css(testNode, { height: '45px' });
     expect(newstyling).to.not.equal(null);
     expect(newstyling.height).to.equal('45px');
   });
 
+  it('Should change the styling before returning the style', () => {
+    testNode.style.height = '10px';
+    const newstyling = css(testNode, { height: '45px' });
+    expect(newstyling.height).to.equal('45px');
+  });
+
   it('Should accept dashed and camelCase property names', () => {
-    const node = helpers.id(testID);
-    const newstyling = css(node, { 'line-height': '20px', fontSize: '20px' });
+    const newstyling = css(testNode, { 'line-height': '20px', fontSize: '20px' });
     expect(newstyling).to.not.equal(null);
     expect(newstyling.lineHeight).to.equal('20px');
     expect(newstyling.fontSize).to.equal('20px');
   });
-
-  it('Should get the styling of a pseudo element', () => {
-    const node = helpers.id(testID);
-    const pseudo = css(node, 'after');
-    expect(pseudo).to.exist;
-    expect(pseudo.content).to.equal('"after"');
-    expect(css(node, 'after', 'content')).to.equal('after');
-  });
-
-  // it('Should return null if there is no pseudo element', () => {
-  //   const node = helpers.id(testID);
-  //   const pseudo = css(node, 'before');
-  //   expect(pseudo).to.equal(null);
-  // });
 
   it('Should return null on non DOM elements', () => {
     expect(css(null)).to.equal(null);
