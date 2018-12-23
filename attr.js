f4c4c2c0dd90659e1868f8a2e0fb3b9b4cab337c
Toke Voltelen <tokevoltelen@gmail.com>
2018-12-23 11:43:05 +0100

    Optimize `attr`

--- attr.js
+++ attr.js
@@ -1,5 +1,7 @@
 import isDOMElement from './isDOMElement';
 
+
+
 /**
  * Get/set the value of an attribute on a given DOM element
  * @function attr
@@ -9,15 +11,14 @@ import isDOMElement from './isDOMElement';
  * @return {String} Data found in the attribute (the old value if {value} is defined)
  */
 export default function attr(elm, attrName, value) {
-  if(!isDOMElement(elm) || !attrName) { return false; }
+  if (!isDOMElement(elm) || !attrName) { return; }
 
   const oldVal = elm.getAttribute(attrName);
-  if(value === true) { value = ''; }
 
-  if(value === false) {
+  if (value === false) {
     elm.removeAttribute(attrName);
-  } else if(typeof value !== 'undefined') {
-    elm.setAttribute(attrName, value);
+  } else if (typeof value !== 'undefined') {
+    elm.setAttribute(attrName, value === true ? '' : value);
   }
 
   return oldVal;
