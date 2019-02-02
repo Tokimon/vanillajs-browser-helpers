f23f245e11775a372573ba400d38cbd6d27a8e92
Toke Voltelen <tokevoltelen@gmail.com>
2019-02-02 17:35:55 +0100

    Correct `children`

--- children.js
+++ children.js
@@ -3,9 +3,9 @@ import isDOMContainer from './isDOMContainer';
 /**
  * Find the children of a given DOM element
  * @function children
- * @param {HTMLElement} [elm=document.body] - DOM element to find children from
+ * @param {HTMLElement} - DOM element to find children from
  * @return {HTMLElement[]} List of found child DOM elements
  */
-export default function children(elm = document.body) {
+export default function children(elm) {
   return isDOMContainer(elm) ? Array.from(elm.children) : [];
 }
--- test/children.spec.js
+++ test/children.spec.js
@@ -9,12 +9,14 @@ const testID = 'TestNode';
 
 
 describe('"children"', () => {
-  before(() => helpers.html(`<div id="${testID}">
-  <span><b></b></span>
-  text
-  <br>
-  <span><i></i><span>
-</div>`));
+  before(() => helpers.html(`
+    <div id="${testID}">
+      <span><b></b></span>
+      text
+      <br>
+      <span><i></i><span>
+    </div>
+  `));
 
   after(() => helpers.remove(testID));
 
@@ -22,34 +24,25 @@ describe('"children"', () => {
     const node = helpers.id(testID);
     const childs = children(node);
 
-    expect(Array.isArray(childs)).to.equal(true);
-    expect(childs.length).to.equal(3);
+    expect(childs).to.be.array().ofSize(3);
   });
 
   it('Should return an empty Array if a non DOM element is passed in as elm argument', () => {
-    let childs = children(null);
-
-    expect(Array.isArray(childs)).to.equal(true);
-    expect(childs.length).to.equal(0);
-
-    childs = children({});
-
-    expect(Array.isArray(childs)).to.equal(true);
-    expect(childs.length).to.equal(0);
+    expect(children()).to.equalTo([]);
+    expect(children({})).to.equalTo([]);
   });
 
   it('Should return child elements of an element not yet in the DOM', () => {
     const div = helpers.create('div');
     div.innerHTML = `
-    <span><b></b></span>
-    text
-    <br>
-    <span><i></i><span>
+      <span><b></b></span>
+      text
+      <br>
+      <span><i></i><span>
     `;
 
     const childs = children(div);
 
-    expect(Array.isArray(childs)).to.equal(true);
-    expect(childs.length).to.equal(3);
+    expect(childs).to.be.array().ofSize(3);
   });
 });
