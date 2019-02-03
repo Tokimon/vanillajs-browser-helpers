d1c2b293ecae99711ccba0d33ae04b4c8414d2f6
Toke Voltelen <tokevoltelen@gmail.com>
2019-02-03 18:57:45 +0100

    update create

--- create.js
+++ create.js
@@ -1,3 +1,5 @@
+import isString from 'vanillajs-helpers/isString';
+
 import selectorToHTML from './selectorToHTML';
 import toDOM from './toDOM';
 
@@ -6,10 +8,12 @@ import toDOM from './toDOM';
 /**
  * Creates an element from a given CSS selector (restricted to only one element)
  * @function create
- * @param  {String} selector - The CSS selector to convert
- * @return {HTMLElement} The converted element
+ * @param {String} [selector = 'div'] - The CSS selector to convert
+ * @return {HTMLElement} The created element
  */
 export default function create(selector) {
+  if (!isString(selector)) { selector = 'div'; }
+
   return /^[a-z]+$/i.test(selector)
     ? document.createElement(selector)
     : toDOM(selectorToHTML(selector))[0];
