fe641f21cc84e54148f072c65db924c82f324602
Toke Voltelen <tokevoltelen@gmail.com>
2019-02-03 19:18:45 +0100

    Correct matches

--- matches.js
+++ matches.js
@@ -1,27 +1,28 @@
-import prefixed from './prefixed';
+import vendorPrefixed from './vendorPrefixed';
 import inDOM from './inDOM';
 
-// Determine the supported method of 'matches' (with or without prefixes)
 function fallback(selector) {
   const matches = document.querySelectorAll(selector);
   let i = matches.length;
-  while(--i >= 0 && matches.item(i) !== this) {} // eslint-disable-line no-empty
+  while (--i >= 0 && matches.item(i) !== this) {} // eslint-disable-line no-empty
   return i > -1;
 }
 
-const _matchMethod = Element.matches ||
+// Determine the supported method of 'matches' (with or without prefixes)
+const _match = Element.matches ||
   Element.matchesSelector ||
-  Element[prefixed('MatchesSelector').filter((method) => !!Element[method])[0]] ||
+  Element[vendorPrefixed('MatchesSelector').filter((method) => !!Element[method])[0]] ||
   fallback;
 
 /**
  * Determines whether or not a DOM element matches a given CSS query selector
+ *
  * @function matches
  * @param {DOM element} elm - DOM element to test
  * @param {String} selector - CSS selector {elm} should match
  * @return {Boolean} Whether or not {elm} matched the selector
  */
 export default function matches(elm, selector = '') {
-  if(!inDOM(elm)) { return false; }
-  return _matchMethod.call(elm, selector);
+  if (!inDOM(elm)) { return false; }
+  return _match.call(elm, selector);
 }
--- test/matches.spec.js
+++ test/matches.spec.js
@@ -12,22 +12,27 @@ describe('"matches"', () => {
   before(() => helpers.html(`<div id="${testID}"><span class="class"><b></b></span></div>`));
   after(() => helpers.remove(testID));
 
-  it('Should indicate if a DOM element matches a given CSS selector', () => {
-    const node = helpers.id(testID);
-
-    // expect(matches(node, '#testID')).to.equal(true);
-    expect(matches(node, '.class')).to.equal(false);
-    expect(matches(node.firstChild, '#testID')).to.equal(false);
-    // expect(matches(node.firstChild, '.class')).to.equal(true);
-    expect(matches(node.firstChild.firstChild, '#testID')).to.equal(false);
-    expect(matches(node.firstChild.firstChild, '.class')).to.equal(false);
-    // expect(matches(document.body, 'body')).to.equal(true);
-    expect(matches(document.body, 'html')).to.equal(false);
-  });
-
   it('Should return false for non DOM elements', () => {
     expect(matches(null)).to.equal(false);
     expect(matches({})).to.equal(false);
     expect(matches()).to.equal(false);
+  });
+
+  it('Should return true if a DOM element matches a given CSS selector', () => {
+    const node = helpers.id(testID);
+
+    expect(matches(document.body, 'body')).to.equal(true, 'body');
+    expect(matches(node, '#' + testID)).to.equal(true, 'id');
+    expect(matches(node.firstChild, '.class')).to.equal(true, 'class');
+  });
+
+  it('Should return false if a DOM element does not match a given CSS selector', () => {
+    const node = helpers.id(testID);
+
+    expect(matches(node, '.class')).to.equal(false);
+    expect(matches(node.firstChild, '#' + testID)).to.equal(false);
+    expect(matches(node.firstChild.firstChild, '#' + testID)).to.equal(false);
+    expect(matches(node.firstChild.firstChild, '.class')).to.equal(false);
+    expect(matches(document.body, 'html')).to.equal(false);
   });
 });
