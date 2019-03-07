c803a0fa3e7b693f00626e444fc05ec2d34146b3
Toke Voltelen <tokevoltelen@gmail.com>
2019-03-07 19:12:43 +0100

    improve innerXML

--- innerXML.js
+++ innerXML.js
@@ -1,5 +1,17 @@
+/**
+ * Gets the inner XML structure as a string from a XML element
+ * (like innerHTML but for XML elements - eg. in SVG)
+ *
+ * @function innerXML
+ * @param  {Node} XMLNode - The XML node to grab the inner XML structure from
+ * @return {String} - The inner XML structure
+ */
 export default function innerXML(XMLNode) {
-  if('innerHTML' in XMLNode) { return XMLNode.innerHTML; }
+  if (!(XMLNode instanceof Node)) { return ''; }
+
+  if (XMLNode.innerHTML !== void 0) {
+    return XMLNode.innerHTML;
+  }
 
   const serializer = new XMLSerializer();
 
