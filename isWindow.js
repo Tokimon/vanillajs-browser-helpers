e89f925f49801b7d86ed6df80ebdd699b61a51c2
Toke Voltelen <tokevoltelen@gmail.com>
2019-03-09 21:19:40 +0100

    Improve isWindow

--- isWindow.js
+++ isWindow.js
@@ -1,10 +1,14 @@
+import objectType from 'vanillajs-helpers/objectType';
+
+
+
 /**
  * Is the given object a Window object
+ *
  * @function isWindow
  * @param {Object} obj - The object to check
  * @return {Boolean} Is it a Window object or not
  */
 export default function isWindow(obj) {
-  if(!obj || !obj.self) { return false; }
-  return obj instanceof obj.self.Window;
+  return !!obj && (!!obj.self || objectType(obj) === 'window');
 }
