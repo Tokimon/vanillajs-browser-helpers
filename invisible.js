a5f8f6a1860c74bf46296650e1731f4135ced64f
Toke Voltelen <tokevoltelen@gmail.com>
2019-03-10 23:19:09 +0100

    Improve invisible

--- invisible.js
+++ invisible.js
@@ -2,19 +2,24 @@ import hidden from './hidden';
 
 /**
  * Test if a given DOM element is invisibile.
+ *
  * @function invisibile
  * @param {HTMLElement} elm - DOM element to test
  * @return {Boolean} Is the element invisible
  */
 export default function invisible(elm) {
-  if(hidden(elm)) { return true; }
+  do {
+    if (
+      hidden(elm) ||
+      !elm.offsetHeight ||
+      !elm.offsetWidth ||
+      !Number(getComputedStyle(elm).opacity)
+    ) {
+      return true;
+    }
 
-  // In collapsed or transparent elements we have to look at the element and its
-  // parents to see if we find one that is invisible
-  while(elm) {
-    if(hidden(elm) || !Number(getComputedStyle(elm).opacity)) { return true; }
     elm = elm.parentElement;
-  }
+  } while (elm);
 
   return false;
 }
