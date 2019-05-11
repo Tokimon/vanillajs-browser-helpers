c2927ccce4a1ef726165d2d79e09ae2b141905b2
Toke Voltelen <tokevoltelen@gmail.com>
2019-05-11 18:54:32 +0200

    Update scrollParent

--- scrollParent.js
+++ scrollParent.js
@@ -1,17 +1,24 @@
 import viewport from './viewport';
 import isDOMChildNode from './isDOMChildNode';
 
+
+
+/**
+ * Get the parent element that has scrolling
+ * @param  {HTMLElement} elm - The element whose scroll parent is determined
+ * @return {HTMLElement} The scroll parent or the viewport
+ */
 export default function scrollParent(elm) {
   const { position: elmPosition } = getComputedStyle(elm);
 
-  if(isDOMChildNode(elm) && elmPosition !== 'fixed') {
+  if (isDOMChildNode(elm) && elmPosition !== 'fixed') {
     const noStaticParent = elmPosition === 'absolute';
     let parent = elm;
 
-    while(parent && parent !== document.body) {
+    while (parent && parent !== document.body) {
       const { position, overflow, overflowX, overflowY } = getComputedStyle(parent);
 
-      if(
+      if (
         !(noStaticParent && position === 'static') &&
         /(auto|scroll)/.test(overflow + overflowY + overflowX)
       ) {
