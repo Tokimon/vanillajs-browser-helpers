abae941841a3dcf2a61c8d4aa19637013295b15b
Toke Voltelen <tokevoltelen@gmail.com>
2019-05-11 18:57:57 +0200

    toDOM: Improve and complete tests

--- test/toDOM.spec.js
+++ test/toDOM.spec.js
@@ -5,29 +5,145 @@ import toDOM from '../toDOM';
 
 
 describe('"toDOM" >', () => {
-  it('Should generate DOM elements from a HTML string', () => {
-    const dom = toDOM(`
-      <p class="item">Text content</p>
-      <div id='Item'><b></b><b></b><b></b></div>
-      <p class="item">Text content 2</p>
+  it.each(
+    [
+      undefined,
+      null,
+      123,
+      {},
+      [],
+      helpers.create('p'),
+      '<html />',
+      '<head />',
+      '<body />',
+      '<frame />',
+      '<frameset />'
+    ],
+    '%s should be returned as is',
+    (val) => { expect(toDOM(val)).to.equal(val); }
+  );
+
+  it.each(
+    [
+      '<a />',
+      '<abbr />',
+      '<acronym />',
+      '<address />',
+      '<applet />',
+      '<area />',
+      '<article />',
+      '<aside />',
+      '<audio />',
+      '<b />',
+      '<base />',
+      '<basefont />',
+      '<bdo />',
+      '<big />',
+      '<blockquote />',
+      '<br />',
+      '<button />',
+      '<canvas />',
+      '<caption />',
+      '<center />',
+      '<cite />',
+      '<code />',
+      '<col />',
+      '<colgroup />',
+      '<datalist />',
+      '<dd />',
+      '<del />',
+      '<dfn />',
+      '<div />',
+      '<dl />',
+      '<dt />',
+      '<em />',
+      '<embed />',
+      '<fieldset />',
+      '<figcaption />',
+      '<figure />',
+      '<font />',
+      '<footer />',
+      '<form />',
+      '<header />',
+      '<h1 />',
+      '<h2 />',
+      '<h3 />',
+      '<h4 />',
+      '<h5 />',
+      '<h6 />',
+      '<hr />',
+      '<i />',
+      '<iframe />',
+      '<img />',
+      '<input />',
+      '<ins />',
+      '<kbd />',
+      '<label />',
+      '<legend />',
+      '<li />',
+      '<link />',
+      '<main />',
+      '<map />',
+      '<mark />',
+      '<meta />',
+      '<meter />',
+      '<nav />',
+      '<noscript />',
+      '<object />',
+      '<ol />',
+      '<optgroup />',
+      '<option />',
+      '<p />',
+      '<param />',
+      '<pre />',
+      '<progress />',
+      '<q />',
+      '<s />',
+      '<samp />',
+      '<script />',
+      '<section />',
+      '<select />',
+      '<small />',
+      '<source />',
+      '<span />',
+      '<strike />',
+      '<strong />',
+      '<style />',
+      '<sub />',
+      '<sup />',
+      '<table />',
+      '<tbody />',
+      '<td />',
+      '<textarea />',
+      '<tfoot />',
+      '<th />',
+      '<thead />',
+      '<time />',
+      '<title />',
+      '<tr />',
+      '<u />',
+      '<ul />',
+      '<var />',
+      '<video />',
+      '<wbr />'
+    ],
+    'Tag "%s" should be converted to a dom element',
+    (tag) => {
+      const tagName = tag.slice(1, tag.length - 3).toUpperCase();
+      expect(toDOM(tag)[0].tagName).to.equal(tagName);
+    }
+  );
+
+  it('Should return a HTMLCollection of all root elements converted', () => {
+    const nodes = toDOM(`
+      <div></div>
+      <p></p>
+      <a></a>
     `);
 
-    expect(dom).to.be.a('array').and.to.have.length(3);
-
-    const Item = dom[1];
-    const items = dom.filter((elm) => elm.className === 'item');
-
-    expect(Item).to.exist.and.to.have.id('Item');
-    expect(Item).to.contain('b');
-    expect(Item.childNodes).to.have.length(3);
-    expect(items).to.have.length(2);
-    expect(items[0]).to.have.attribute('class', 'item');
-    expect(items[0].textContent).to.equal('Text content');
-  });
-
-  it('Should return non String inputs as they were', () => {
-    expect(toDOM()).to.equal(undefined);
-    expect(toDOM(null)).to.equal(null);
-    expect(toDOM(helpers.create('p')).tagName).to.equal('P');
+    expect(nodes instanceof HTMLCollection).to.equal(true);
+    expect(nodes[0].tagName).to.equal('DIV');
+    expect(nodes[1].tagName).to.equal('P');
+    expect(nodes[2].tagName).to.equal('A');
   });
 });
