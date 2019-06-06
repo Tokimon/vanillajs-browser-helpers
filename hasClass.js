792abdb0eadd912c75898d52f22aca3c2ed0778b
Toke Voltelen <tokevoltelen@gmail.com>
2019-06-06 10:41:18 +0200

    Fix jsdoc

--- hasClass.js
+++ hasClass.js
@@ -7,7 +7,7 @@ import isArray from 'vanillajs-helpers/isArray';
 /**
  * Does all (or any) of the listed class names exist in the DOM elements list of class names
  * @function hasClass
- * @param {DOM element} elm - DOM element to test
+ * @param {HTMLElement} elm - DOM element to test
  * @param {String|String[]} classNames - Class names to test
  * @param {Boolean} [any] - Test if at least one class name exist
  * @return {Boolean} All/any class names listed were found in the elements list of class names
@@ -26,7 +26,7 @@ export default function hasClass(elm, classNames, any) {
 /**
  * Does any of the listed class names exist in the DOM elements list of class names
  * @function hasAnyClass
- * @param {DOM element} elm - DOM element to test
+ * @param {HTMLElement} elm - DOM element to test
  * @param {String|String[]} classNames - Class names to test
  * @return {Boolean} At least one of the class names listed were found in the elements list of class names
  */
--- tasks/jsdoc.js
+++ tasks/jsdoc.js
@@ -3,10 +3,10 @@
 const nPath = require('path');
 const cp = require('child_process');
 const fs = require('fs-promise');
-const glob = require('glob-promise');
+const glob = require('globby');
 
 fs.remove(nPath.resolve('jsdoc'))
-  .then(() => glob('./!(karma)*.js'))
+  .then(() => glob('./!(karma|webpack)*.js'))
   .then((files) => {
     files = files.map((file) => nPath.relative(process.cwd(), file));
     return new Promise((resolve, reject) =>
