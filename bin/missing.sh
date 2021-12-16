42a486fab5d07feeabf1e00e4680d8a5798fcd88
Toke Voltelen <tokevoltelen@gmail.com>
2021-12-16 15:55:27 +0100

    Evo | Index file build (#64)
    * Move tests form "src/test" to "src/tests"
    
    * Update bin shell scripts
    
    * Add typings and main entry to package.json
    
    * Update dependencies
    
    * Fixed event Handler helper function
    
    Co-authored-by: Tokimon <tvo@leapwork.com>

--- bin/missing.sh
+++ bin/missing.sh
@@ -1,8 +1,17 @@
+missing=0
+
 for file in ./src/*.ts; do
   b=$(basename $file .ts)
-  name=./src/test/$b.test.ts;
+  testfile=./src/tests/$b.test.ts;
 
-  if [ ! -f $name ]; then
+  if [ ! -f $testfile ]; then
+    missing=$(expr $missing + 1)
     echo $b
   fi
 done
+
+if [ $missing = 0 ]; then
+  echo -e "\e[1;32m🗸\e[0m All good! There are no missing tests"
+else
+  echo -e "\e[1;31m$missing missing tests\e[0m"
+fi
