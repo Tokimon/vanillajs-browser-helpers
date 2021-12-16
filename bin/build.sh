5fe1652cff61eb8eaf79e07fadacb59bf785f82e
Tokimon <tvo@leapwork.com>
2021-12-16 11:08:57 +0100

    Update bin shell scripts

--- bin/build.sh
+++ bin/build.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-build() {
-  printf 'Building .%s ... ' $1
+print_done() {
+  echo -e '\e[1;32mdone\e[0m'
+}
 
+print_building() {
+  printf "Building \e[1;33m.$1\e[0m ... "
+}
+
+build() {
+  print_building $1
+  
   # Compile the typescript files
   npx tsc -p tsconfig.$1.json
 
@@ -13,14 +21,22 @@ build() {
 
   rmdir ./$1
 
-  echo 'done'
+  print_done
 }
+
+# Preparing the accumulated index file
+rm -f ./src/index.ts
+
+for file in ./src/*.ts; do
+  name=$(basename $file .ts)
+  echo "export { default as $name } from './$name';" >> ./src/index.ts
+done
 
 build mjs
 build cjs
 
-printf 'Building .js + .d.ts ... '
+print_building '.js\e[0m and \e[1;33m.d.ts'
 
 npx tsc -p tsconfig.json
 
-echo 'done'
+print_done
--- bin/clean.sh
+++ bin/clean.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ok() {
-  echo '🗸' $1 'removed'
+  echo -e "\e[1;32m🗸\e[0m \e[1;33m$1\e[0m removed"
 }
 
 rm -f ./*.mjs
@@ -16,7 +16,11 @@ for file in ./src/*.ts; do
   rm -f ./$b.d.ts
 done
 
-ok '.js and .d.ts'
+ok '.js\e[0m and \e[1;33m.d.ts'
+
+rm -f ./src/index.ts
+
+ok 'generated index file'
 
 find . -type d -empty -delete
 
